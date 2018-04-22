03c0b67472d63c62433b5f2e3f5bcf1c623f7f0e
Ziyan Wang <zw301@nyu.edu>
2018-04-21 18:40:19 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+// dont know if this one is working
+// import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
+
+//localhost:5000
