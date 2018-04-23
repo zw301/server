285c787d9b887969fea4e41d4dc5823dbce5ae38
Ziyan Wang <zw301@nyu.edu>
2018-04-22 17:55:59 -0700

    finished auth flow

--- config/keys.js
+++ config/keys.js
@@ -2,7 +2,7 @@
 
 if (process.env.NODE_ENV === "production") {
   // we are in production - return the prod set of keys
-  models.exports = require("./prod");
+  module.exports = require("./prod");
 } else {
   // we are in development - return the dev keys
   module.exports = require("./dev");
